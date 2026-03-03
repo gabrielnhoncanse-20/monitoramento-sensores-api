1b65326c278fa9a40e7b5f9fcf6f294d82d14a60
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-03 20:31:49 -0300

    feat: estrutura inicial do projeto Spring Boot com JPA, H2 e Lombok

---
+++ demo/src/test/java/com/fiap/mecatronica/demo/MonitoramentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.mecatronica.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitoramentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
